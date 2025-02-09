0f1190f61ca7186bdd03f6be90468e1028f7b687
prakash <10prakashgautam@gmail.com>
2025-02-09 14:59:28 -0500

    lab3b.py

---
+++ ops445/lab3/lab3b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Lab 3 Part 1 script - functions'''
+# Author ID: [seneca_id] 
+
+def sum_numbers(number1, number2):
+    return number1 + number2
+
+def subtract_numbers(number1, number2):
+    return number1 - number2
+
+def multiply_numbers(number1, number2):
+    return number1 * number2
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
