c6cf703a3832db44ee6d4ed466371fe9eea17f08
prakash <10prakashgautam@gmail.com>
2025-02-09 14:53:20 -0500

    lab3a.py

---
+++ ops445/lab3/lab3a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [seneca_id]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+    return 15
+
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
